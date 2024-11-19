b7b8c5bd53d4ce5ad05504b489bc4fc4e7effd2c
Anthony Li <antli@seas.upenn.edu>
2024-11-19 16:16:24 -0500

    Switch implementation to RealityView

--- Lootbox Legends/View Models/LootboxViewModel.swift
+++ Lootbox Legends/View Models/LootboxViewModel.swift
@@ -6,60 +6,62 @@
 //
 
 import SwiftUI
-import ARKit
 import RealityKit
+import OSLog
 
-class LootboxViewModel: ObservableObject {
-    @Published var currentItem: LootboxItem?
-    @Published var showUnableToPlaceMessage = false
+private let logger = Logger(subsystem: "Lootbox Legends", category: "LootboxViewModel")
+
+@Observable @MainActor class LootboxViewModel {
+    var currentItem: LootboxItem?
+    var showUnableToPlaceMessage = false
     
-    var arView: ARView?
+    var realityViewContent: RealityViewCameraContent?
     var anchor: AnchorEntity?
     
-    let lootboxTemplate: Entity
+    var lootboxTemplate: Entity?
     let feedbackGenerator = UIImpactFeedbackGenerator(style: .light)
     
-    init() {
-        lootboxTemplate = try! Entity.load(named: "lootbox")
+    func setUp(in realityViewContent: RealityViewCameraContent) async {
+        self.realityViewContent = realityViewContent
+        
+        let lootboxTemplate = try! await ModelEntity(named: "lootbox")
+        self.lootboxTemplate = lootboxTemplate
+        
         lootboxTemplate.components.set(CollisionComponent(shapes: [.generateBox(width: 0.2, height: 0.13, depth: 0.1)]))
         
-        var physicsBodyComponent = PhysicsBodyComponent()
-        physicsBodyComponent.massProperties.mass = 0.5
-        physicsBodyComponent.mode = .dynamic
-        lootboxTemplate.components.set(physicsBodyComponent)
-    }
-    
-    func setup(in arView: ARView) {
-        self.arView = arView
+        var lootboxPhysicsBodyComponent = PhysicsBodyComponent()
+        lootboxPhysicsBodyComponent.massProperties.mass = 0.5
+        lootboxPhysicsBodyComponent.mode = .dynamic
+        lootboxTemplate.components.set(lootboxPhysicsBodyComponent)
         
         // Create horizontal table anchor for the content
         let anchor = AnchorEntity(.plane(.horizontal, classification: .table, minimumBounds: SIMD2<Float>(0.2, 0.2)))
         anchor.components.set(CollisionComponent(shapes: [.generateBox(width: 100, height: 0.001, depth: 100)]))
         
         // Add a physics body for the table
-        var physicsBodyComponent = PhysicsBodyComponent()
-        physicsBodyComponent.mode = .static
-        anchor.components.set(physicsBodyComponent)
+        var tablePhysicsBodyComponent = PhysicsBodyComponent()
+        tablePhysicsBodyComponent.mode = .static
+        anchor.components.set(tablePhysicsBodyComponent)
         
         // Add the anchor to the scene and view model
-        arView.scene.anchors.append(anchor)
-        arView.physicsOrigin = anchor
+        realityViewContent.entities.append(anchor)
         self.anchor = anchor
     }
     
-    func addLootbox() {
-        guard let anchor, let arView else {
+    func addLootbox(at point: CGPoint) {
+        guard let anchor, let realityViewContent, let lootboxTemplate else {
             return
         }
         
-        let hits = arView.hitTest(arView.center, query: .nearest)
-        guard let hit = hits.first else {
+        let hits = realityViewContent.hitTest(point: point, in: .local, query: .all)
+        guard let hit = hits.first(where: { $0.entity === anchor }) else {
             showUnableToPlaceMessage = true
             return
         }
         
         let position = anchor.convert(position: hit.position, from: nil)
         let lootbox = lootboxTemplate.clone(recursive: true)
+        logger.debug("Hit test with \(hit.entity.debugDescription) at \(hit.position) -> \(position)")
         lootbox.position = position + SIMD3(x: 0, y: 0.5, z: 0)
         lootbox.components.set(LootboxComponent(requiredTaps: Int.random(in: 2..<11)))
         anchor.addChild(lootbox)
@@ -68,11 +70,11 @@ class LootboxViewModel: ObservableObject {
     }
     
     func handleTap(at position: CGPoint) {
-        guard let arView else {
+        guard let realityViewContent else {
             return
         }
         
-        let hits = arView.hitTest(position, query: .all)
+        let hits = realityViewContent.hitTest(point: position, in: .local, query: .all)
         guard let hit = hits.first(where: { $0.entity.components.has(LootboxComponent.self) }) else {
             return
         }
